0b07f81aeb92c7c72c4e7eedfd42640ba7d6a782
hascode <hascode@localhost>
2012-11-18 15:03:33 +0100

    TestWatcher example added.

---
+++ src/test/java/com/hascode/tutorial/WatcherExampleTest.java
@@ -0,0 +1,33 @@
+package com.hascode.tutorial;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeFalse;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.internal.AssumptionViolatedException;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+public class WatcherExampleTest {
+	@Rule
+	public TestWatcher watcher = new TestWatcher() {
+		@Override
+		protected void skipped(final AssumptionViolatedException e,
+				final Description description) {
+			System.out.println("method " + description.getMethodName()
+					+ " was skipped because of '" + e.getMessage() + "'");
+		}
+	};
+
+	@Test
+	public void testSomething() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testIgnored() {
+		assumeFalse(true);
+	}
+
+}
